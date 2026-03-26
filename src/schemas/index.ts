853f51cc68a082993c6d8fb9dd25d1828cdb0373
Vijay Hardaha <vijayhardaha@proton.me>
2026-03-27 01:01:18 +0530

    fix: rename tmp files to correct filenames

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,6 +2,6 @@ export { personSchema, type PersonOptions } from './person';
 export { organizationSchema, type OrganizationOptions } from './organization';
 export { websiteSchema, type WebSiteOptions } from './webSite';
 export { webPageSchema, aboutPageSchema, contactPageSchema, type WebPageOptions } from './webPage';
-export { webApiSchema, type WebApiOptions } from './webAPI';
+export { webApiSchema, type WebApiOptions } from './webApi';
 export { softwareAppSchema, type SoftwareAppOptions } from './softwareApp';
 export { breadcrumbSchema, type BreadcrumbOptions, type BreadcrumbItem } from './breadCrumb';
--- src/schemas/webApi.test.ts
+++ src/schemas/webApi.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { webApiSchema } from './webAPI';
+import { webApiSchema } from './webApi';
 
 // describe: Tests for webApiSchema
 describe('webApiSchema', () => {
